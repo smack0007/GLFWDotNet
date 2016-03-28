068492e9acb8a0af9222b103afb88ef7205e3689
smack0007 <zachary.snow@gmail.com>
2016-03-28 21:04:53 +0200

    HelloWorld sample now shows the renderer string in the title.

--- Source/Samples/HelloWorld/Program.cs
+++ Source/Samples/HelloWorld/Program.cs
@@ -1,10 +1,15 @@
 ﻿using System;
+using System.Runtime.InteropServices;
 using GLFWDotNet;
 
 namespace HelloWorld
 {
     public static class Program
     {
+        private const uint RENDERER = 0x1F01;
+        
+        private delegate IntPtr GetString(uint name);
+
         public static int Main(string[] args)
         {
             if (GLFW.Init() == 0)
@@ -12,6 +17,11 @@ namespace HelloWorld
 
             int major, minor, revision;
             GLFW.GetVersion(out major, out minor, out revision);
+
+            GLFW.WindowHint(GLFW.CLIENT_API, GLFW.OPENGL_API);
+            GLFW.WindowHint(GLFW.OPENGL_PROFILE, GLFW.OPENGL_CORE_PROFILE);
+            GLFW.WindowHint(GLFW.CONTEXT_VERSION_MAJOR, 3);
+            GLFW.WindowHint(GLFW.CONTEXT_VERSION_MINOR, 3);
 
             string title = $"Hello World! GLFW {major}.{minor}.{revision}";
 
@@ -24,9 +34,17 @@ namespace HelloWorld
 
             GLFW.MakeContextCurrent(window);
 
+            IntPtr proc = GLFW.GetProcAddress("glGetString");
+            var getString = (GetString)Marshal.GetDelegateForFunctionPointer(proc, typeof(GetString));
+            var rendererString = Marshal.PtrToStringAnsi(getString(RENDERER));
+
+            GLFW.SetWindowTitle(window, title + " " + rendererString);
+
             while (GLFW.WindowShouldClose(window) == 0)
             {
                 GLFW.PollEvents();
+
+                GLFW.SwapBuffers(window);
             }
 
             GLFW.Terminate();

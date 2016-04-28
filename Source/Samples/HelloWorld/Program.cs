0191370c42fe96e25da3369a2b68ce3aa36abd49
smack0007 <zachary.snow@gmail.com>
2016-04-28 21:26:20 +0200

    HelloWorld now reads VERSION string as well.

--- Source/Samples/HelloWorld/Program.cs
+++ Source/Samples/HelloWorld/Program.cs
@@ -7,7 +7,8 @@ namespace HelloWorld
     public static class Program
     {
         private const uint RENDERER = 0x1F01;
-        
+        private const uint VERSION = 0x1F02;
+
         private delegate IntPtr GetString(uint name);
 
         public static int Main(string[] args)
@@ -20,8 +21,8 @@ namespace HelloWorld
 
             GLFW.WindowHint(GLFW.CLIENT_API, GLFW.OPENGL_API);
             GLFW.WindowHint(GLFW.OPENGL_PROFILE, GLFW.OPENGL_CORE_PROFILE);
-            GLFW.WindowHint(GLFW.CONTEXT_VERSION_MAJOR, 3);
-            GLFW.WindowHint(GLFW.CONTEXT_VERSION_MINOR, 3);
+            GLFW.WindowHint(GLFW.CONTEXT_VERSION_MAJOR, 4);
+            GLFW.WindowHint(GLFW.CONTEXT_VERSION_MINOR, 0);
 
             string title = $"Hello World! GLFW {major}.{minor}.{revision}";
 
@@ -36,9 +37,10 @@ namespace HelloWorld
 
             IntPtr proc = GLFW.GetProcAddress("glGetString");
             var getString = (GetString)Marshal.GetDelegateForFunctionPointer(proc, typeof(GetString));
+            var versionString = Marshal.PtrToStringAnsi(getString(VERSION));
             var rendererString = Marshal.PtrToStringAnsi(getString(RENDERER));
 
-            GLFW.SetWindowTitle(window, title + " " + rendererString);
+            GLFW.SetWindowTitle(window, title + " " + versionString + " " + rendererString);
 
             while (GLFW.WindowShouldClose(window) == 0)
             {

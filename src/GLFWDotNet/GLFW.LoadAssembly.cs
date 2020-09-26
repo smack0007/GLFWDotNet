07150c504013f292045ef45b8a8966bf7a670008
smack0007 <zachary.snow@gmail.com>
2020-09-26 21:25:52 +0200

    Small style corrections.

--- src/GLFWDotNet/GLFW.LoadAssembly.cs
+++ src/GLFWDotNet/GLFW.LoadAssembly.cs
@@ -17,14 +17,14 @@ namespace GLFWDotNet
 
         private static class Unix
         {
-
             public static IntPtr LoadLibrary(string fileName)
             {
                 IntPtr retVal = dlopen(fileName, 2);
-                var errPtr = dlerror ();
-                if (errPtr != IntPtr.Zero) {
+                var errPtr = dlerror();
+                
+                if (errPtr != IntPtr.Zero)
                     throw new InvalidOperationException(Marshal.PtrToStringAnsi (errPtr));
-                }
+
                 return retVal;
             }
 
@@ -38,12 +38,11 @@ namespace GLFWDotNet
             private static extern IntPtr dlerror();
         }
 
-
         private static Func<string, IntPtr> LoadAssembly()
         {
             var assemblyDirectory = Path.GetDirectoryName(typeof(GLFW).Assembly.Location);
 
-            if(RuntimeInformation.IsOSPlatform(OSPlatform.Windows))
+            if (RuntimeInformation.IsOSPlatform(OSPlatform.Windows))
             {
                 string assemblyPath = Path.Combine(
                     assemblyDirectory,
@@ -60,13 +59,15 @@ namespace GLFWDotNet
                 return x => Win32.GetProcAddress(assembly, x);
             }
 
-            if(RuntimeInformation.IsOSPlatform(OSPlatform.Linux) || RuntimeInformation.IsOSPlatform(OSPlatform.OSX))
+            if (RuntimeInformation.IsOSPlatform(OSPlatform.Linux) || RuntimeInformation.IsOSPlatform(OSPlatform.OSX))
             {
                 string extension = RuntimeInformation.IsOSPlatform(OSPlatform.Linux) ? "so" : "dylib";
                 string assemblyPath = $"libglfw.{extension}";
+                
                 IntPtr assembly = Unix.LoadLibrary(assemblyPath);
+
                 if (assembly == IntPtr.Zero)
-                    throw new InvalidOperationException($"Failed to load GLFW so from path '{assemblyPath}'.");
+                    throw new InvalidOperationException($"Failed to load GLFW {extension} from path '{assemblyPath}'.");
 
                 return functionName => Unix.dlsym(assembly, functionName);
             }
